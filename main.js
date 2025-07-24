7794c00ba0cd063c6f405cc61d5e46f888080909
Kevin Reiner <kevin.reiner@fraud0.com>
2025-07-24 10:55:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const menuBars = document.getElementById('menu-bars');
+const myNav = document.getElementById('my-nav');
+const hideBtns = document.querySelectorAll('.nav-button');
+const hideStart = document.getElementById('hide-1');
+const hideProjects = document.getElementById('hide-2');
+const hideContact = document.getElementById('hide-5');
+  
+let navigation = false; // Flag, um den aktuellen Zustand der Navigation zu speichern
+  
+    function toggleNav() {
+    // Toggle der 'change'-Klasse für die Animation der Menü-Bars
+    menuBars.classList.toggle('change');
+
+    // Navigation-Höhe wechseln basierend auf dem Zustand
+    if (navigation) {
+      myNav.style.height = "70px";
+      hideBtns.forEach(btn => btn.classList.add ('hide'));
+
+    } else {
+        myNav.style.height = "100%";
+        hideBtns.forEach(btn => btn.classList.remove ('hide'));
+      }
+      
+    // Zustand umkehren
+    navigation = !navigation;
+    }
+
+// Event Listeners
+menuBars.addEventListener('click', toggleNav);
+hideStart.addEventListener('click',toggleNav);
+hideProjects.addEventListener('click',toggleNav);
+hideContact.addEventListener('click',toggleNav);
+ 
